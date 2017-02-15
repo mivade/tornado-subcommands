363ce5fe7de24c6b9f5c8acf5b349bdc92457cee
Michael V. DePalatis <mike@depalatis.net>
2017-02-15 16:05:43 -0500

    Add Subparser class for convenience

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 from tornado.options import OptionParser
 
 
+class Subparser(object):
+    """Base class for subparsers.
+
+    :param str name: If given, the string used to execute the subcommand. When
+        not given, the lowercase class name will be used.
+
+    """
+    def __init__(self, name=None):
+        self.options = OptionParser()
+        self.name = name or self.__class__.__name__.lower()
+        self.define_options()
+
+    def define_options(self):
+        """Override to define subparser options."""
+        pass
+
+    def call(self, parent_options, rest):
+        """The command to run when called as a subcommand."""
+
+
 class Parser(OptionParser):
     """Custom :class:`OptionParser` which accepts information about subcommands
     in order to print available subcommands.
@@ -10,21 +30,45 @@ class Parser(OptionParser):
         super().__init__()
         self.__dict__["_subcommands"] = subcommands
 
+    def __getattr__(self, name):
+        if name == "subcommands":
+            return {
+                subcommand.name: subcommand
+                for subcommand in self.__dict__["_subcommands"]
+            }
+        else:
+            return super().__getattr__(name)
+
     def print_help(self, file=None):
         super().print_help(file=file)
         if len(self._subcommands) > 0:
             print("Subcommands:")
             for subcommand in self._subcommands:
-                print(" ", subcommand)
+                assert isinstance(subcommand, Subparser)
+                print(" ", subcommand.name, "\n\t", subcommand.__doc__)
 
 
-main = Parser(subcommands=["subone"])
-subone = OptionParser()
+class Subone(Subparser):
+    """Print all options and exit."""
+    def define_options(self):
+        self.options.define("count", default=1, help="A count")
 
-main.define("verbose", type=bool, default=False, help="Enable verbose output")
+    def call(self, parent_options, rest):
+        self.options.parse_command_line(rest)
+        print(parent_options.as_dict())
+        print(self.options.as_dict())
+        print("subone executed!")
 
-rest = main.parse_command_line()
-print(main.as_dict())
-print(rest)
-if rest[0] == "subone":
-    print("subone executed")
+
+def main():
+    parser = Parser(subcommands=[Subone()])
+    parser.define("verbose", type=bool, default=False,
+                  help="Enable verbose output")
+    rest = parser.parse_command_line()
+
+    if len(rest) > 0:
+        parser.subcommands[rest[0]].call(parser, rest)
+
+
+if __name__ == "__main__":
+    main()
